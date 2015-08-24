e19401c9f749b41871f7f3b2a1dfbda0bac83c12
Dave Yoon <dyoon@dramafever.com>
2015-08-24 09:17:24 -0400

    adding shell scripts to repo for easy updates

---
+++ fix-permissions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chown -R www-data:www-data /usr/share/nginx/design.drama9.com
+chmod -R a+rX /usr/share/nginx/design.drama9.com
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /usr/share/nginx/design.drama9.com
+git pull
+npm install
+bower install
+gulp build
+/usr/share/nginx/design.drama9.com/fix-permissions.sh
