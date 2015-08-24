26db0f24df8f37e4482868c7aa164628f117ae1b
Dave Yoon <dyoon@dramafever.com>
2015-08-24 10:17:04 -0400

    trying to fix things

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 chown -R www-data:www-data /usr/share/nginx/design.drama9.com
 chmod -R a+rX /usr/share/nginx/design.drama9.com
+chmod -R 774 /usr/share/nginx/design.drama9.com/craft/app
+chmod -R 774 /usr/share/nginx/design.drama9.com/craft/config
+chmod -R 774 /usr/share/nginx/design.drama9.com/craft/storage
